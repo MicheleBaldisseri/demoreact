cf06d188c7e02b02efea3bdf62885ab5bd66fb3b
Michele <michele.baldisseri@studenti.unipd.it>
2021-02-14 21:49:58 +0100

    up

--- craco.config.js
+++ craco.config.js
@@ -4,5 +4,8 @@ module.exports = {  //eslint-disable-line
             ["@babel/plugin-proposal-class-properties", { "loose": true }],
             ["@babel/plugin-proposal-nullish-coalescing-operator"]
         ]
+    },
+    jest: {
+        configure: {  testPathIgnorePatterns: ['node_modules/@saehrimnir/druidjs/dist/druid.js']}
     }
 };
